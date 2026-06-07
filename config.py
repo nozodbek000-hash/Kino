6752cae096974572849c6ad9faf9d9e10c2fcaaf
nozodbek000-hash <nozodbek000@gmail.com>
2026-06-07 21:50:42 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,13 @@
+# ========================================
+# CONFIG - Bu yerga o'z ma'lumotlaringizni kiriting
+# ========================================
+
+# BotFather dan olgan tokeningiz
+BOT_TOKEN = "8671506465:AAEczqpZYdRF-pzPvivG7567UaFZCehRYq8"
+
+# Kanal username (@ belgisi bilan)
+CHANNEL_USERNAME = "@kinolar_to"
+
+# Admin ID (o'z Telegram ID ingiz)
+# ID olish uchun @userinfobot ga /start yuboring
+ADMIN_IDS = [5874554565]  # O'z ID ingizni kiriting
