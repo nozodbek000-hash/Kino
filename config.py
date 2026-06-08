a1f3c0ff1f9edc9aa7fda992e47f38ef1c89c444
nozodbek000-hash <nozodbek000@gmail.com>
2026-06-08 16:33:43 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+# config.py
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8671506465:AAEczqpZYdRF-pzPvivG7567UaFZCehRYq8")
+ADMIN_IDS = list(map(int, os.getenv("ADMIN_IDS", "5874554565").split(",")))
+CHANNEL_ID = os.getenv("CHANNEL_ID", "@kinolar_to")
+DB_NAME = os.getenv("DB_NAME", "movies.db")
+PER_PAGE = 8
+STORAGE_CHANNEL  = os.getenv("STORAGE_CHANNEL", "@kinolar_to") # Kino saqlanadigan kanal
